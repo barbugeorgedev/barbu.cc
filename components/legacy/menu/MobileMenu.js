221cf51fac94da3cf339e3384faf73b2f09f79e1
geobar <george.barbu@kindredgroup.com>
2022-06-30 20:59:01 +0200

    step 2

--- components/legacy/menu/MobileMenu.js
+++ components/legacy/menu/MobileMenu.js
@@ -2,11 +2,10 @@ import { bool, object } from "prop-types";
 
 import cn from "classnames";
 import styles from "@styles/mobile-menu.module.css";
-import useDelayedRender from "use-delayed-render";
 
 import MenuItem from './MenuItem';
 
-export default function MobileMenu({ isMenuOpen, style }) {
+export default function MobileMenu({ style }) {
   const menuLinks = [
     { id: "0", href: "/", text: "Home", transitionDelay: "150" },
     { id: "1", href: "/about", text: "About", transitionDelay: "200" },
@@ -14,23 +13,15 @@ export default function MobileMenu({ isMenuOpen, style }) {
     { id: "3", href: "/projects", text: "Projects", transitionDelay: "300" },
   ];
 
-  const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
-    isMenuOpen,
-    {
-      enterDelay: 20,
-      exitDelay: 300,
-    }
-  );
 
   return (
     <nav>
-      {isMenuMounted && (
         <ul
           style={style}
           className={cn(
             styles.menu,
             "flex flex-col absolute bg-slate-100 dark:bg-slate-900",
-            isMenuRendered && styles.menuRendered
+            styles.menuRendered
           )}>
           {menuLinks.map((l) => (
             <MenuItem
@@ -41,17 +32,14 @@ export default function MobileMenu({ isMenuOpen, style }) {
             />
           ))}
         </ul>
-      )}
     </nav>
   );
 }
 
 MobileMenu.defaultProps = {
-  isMenuOpen: false,
   style: {},
 };
 
 MobileMenu.propTypes = {
-  isMenuOpen: bool,
   style: object,
 };
